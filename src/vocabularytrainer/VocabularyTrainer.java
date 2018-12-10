437c4f0e09993df94ee973f329c643a9372fbe27
Tim Hamp <timrouven@web.de>
2018-12-10 19:11:49 +0100

    Initial Commit

---
+++ src/vocabularytrainer/VocabularyTrainer.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vocabularytrainer;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Tim
+ */
+public class VocabularyTrainer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.out.println("Unable to use system layout. Use default layout.");
+        }
+        String dictionaryFile = "de-en.txt";
+        if (args.length >= 1) {
+            System.out.println("Dictionary file not set. Use de-en.txt");
+            dictionaryFile = args[0];
+        }
+        MainWindow mw = new MainWindow(dictionaryFile);
+        mw.setLocationRelativeTo(null);
+        mw.setVisible(true);
+    }
+    
+}
