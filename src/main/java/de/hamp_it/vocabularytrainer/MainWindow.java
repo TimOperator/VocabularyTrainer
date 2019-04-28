344d626d975b9674c5049ada4d56d0bd11d6015e
Tim Hamp <timrouven@web.de>
2019-04-28 18:20:07 +0200

    Make it a maven project resolve #2

--- src/main/java/de/hamp_it/vocabularytrainer/MainWindow.java
+++ src/main/java/de/hamp_it/vocabularytrainer/MainWindow.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vocabularytrainer;
+package de.hamp_it.vocabularytrainer;
 
 import de.hamp_it.EasyuseServerConnector.MainServerConnector;
 import java.awt.Color;
